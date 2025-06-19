99f4e5e59ce460893ab55c209798991f27660225
gusta <gustavorandiiii@gmail.com>
2025-06-19 17:02:49 -0300

    Atualizando classe Memoria e Processador

--- src/modelo/Memoria.java
+++ src/modelo/Memoria.java
@@ -2,7 +2,14 @@ package src.modelo;
 
 public class Memoria { // vai ser utilizada para simular memoria RAM
     private int[] dado; // guarda um valor ou instrução de 32 bits
-    private int tamanho; // número de posições dentro de 'intrução32Bits'
+    private int tamanho; // número de posições dentro de 'intrução32Bits']
+
+
+
+    public Memoria(int tamanho){
+        this.tamanho = tamanho;
+        this.dado = new int[tamanho];
+    }
 
 
     public int lerValor(int endereco){
--- src/simulador/Processador.java
+++ src/simulador/Processador.java
@@ -9,6 +9,15 @@ public class Processador { // controla o ciclo
     private Memoria memoriaDeDdados; // guarda valores DURANTE a execução
     private Registrador registrador; //registradores t0, t1, etc...
     private int PCcontador; // "aponta" para a proxima instrução que vai ser buscada, acrescenta + 4
-    private boolean ativo; // quando encontra o syscall --> false; aitvo == true; inativo == false
+    private boolean ativo; // quando encontra o syscall --> false; ativo == true; inativo == false
 
+
+
+
+    //Criar um construtor que inicie os atributos, falando partirculamente da memoria para definir seu tamanho
+    public Processador(){
+        this.memoriaInstrucao = new Memoria(// colocar tamamnho);
+        this.memoriaDeDdados = new Memoria(// colocar tamanho);
+        // colocar os outros metodos
+    }
 }
