c697dade97b72b47dea19666aad3f7fcb1c9bc0e
gusta <gustavorandiiii@gmail.com>
2025-06-19 15:07:42 -0300

    Atualizando classe Memoria

--- src/modelo/Memoria.java
+++ src/modelo/Memoria.java
@@ -1,6 +1,14 @@
 package src.modelo;
 
 public class Memoria { // vai ser utilizada para simular memoria RAM
-    private int[] instrução32Bits;
+    private int[] instrucao32Bits;
     private int tamanho; // número de posições dentro de 'intrução32Bits'
+
+
+    public int lerValor(int endereco){
+        return instrucao32Bits[endereco]; // retornar o valor na posição do endereço
+    }
+
+
+
 }
