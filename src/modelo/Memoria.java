4e1a347006a046198f16ae3f22423502d4bcdca7
gusta <gustavorandiiii@gmail.com>
2025-06-19 22:17:58 -0300

    Atualizando classe Memoria

--- src/modelo/Memoria.java
+++ src/modelo/Memoria.java
@@ -1,5 +1,9 @@
 package src.modelo;
 
+import java.io.DataInputStream; // interpreta os bytes
+import java.io.FileInputStream; // abre o arquivo dos bytes
+import java.io.IOException;
+
 public class Memoria { // vai ser utilizada para simular memoria RAM
     private int[] dado; // guarda um valor ou instrução de 32 bits
     private int tamanho; // número de posições dentro de 'intrução32Bits']
@@ -24,6 +28,24 @@ public class Memoria { // vai ser utilizada para simular memoria RAM
             throw new IllegalArgumentException("Endereço inválido!!!");
         }
         dado[endereco] = valor;
+    }
+
+
+    public void carregarBinario(){
+        try(DataInputStream dis = new DataInputStream(new FileInputStream('caminhoArquivo'))){ // se der erro fecha o arquivo
+        int i = 0; // inicializando um int i para se referir a memoria
+        while(dis.available() >= 2 && i < dado.length){// dis.available --> retorna o numero de bytes disponiveis
+                                                        // e verifica se ainda tem espaco na memoria com dado.length com indice i
+            int low = dis.readByte() & 0xFF; // 0xFF --> converte o byte para inteiro --> numero entre 0 e 255 --> valores quando lemos um arquivo.bin, evitando o erro de lermos o bit mais a esquerda como um sinal (trasnformando o numero em negativo)
+            int high = dis.readByte() & 0xFF;
+            int instrucao = (high << 8) | low; // o bits de high vão para a mais significativa e junta se com low formando a instrução
+            escreverMemoria(i, instrucao);
+            i++;
+        }
+        } catch(IOException erro){ // verifica se der erro e guarda
+            System.err.println("Erro ao carregar o arquivo" + erro.getMessage());
+        }
+
     }
 
 
