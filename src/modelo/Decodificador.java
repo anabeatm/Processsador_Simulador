9f7eb0f0e01f8dfc14e4993a1e57481b6c141fd4
gusta <gustavorandiiii@gmail.com>
2025-06-21 11:35:02 -0300

    Atualizando classe Memoria e Processador

--- src/modelo/Decodificador.java
+++ src/modelo/Decodificador.java
@@ -1,5 +1,18 @@
 package src.modelo;
 
+import src.util.FuncaoExtracaoBits;
+
 public class Decodificador {
     // função principal vai transformar o codigo em binário em um objeto do tipo Intrucao
+
+
+
+
+
+    public Instrucao decodificar(int binario){
+        Instrucao instrucao = new Instrucao();
+        int tipo = FuncaoExtracaoBits.extract_bits ((short)binario, 15, 1);
+
+
+    }
 }
